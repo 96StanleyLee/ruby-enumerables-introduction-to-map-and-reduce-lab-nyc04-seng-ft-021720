d9313c3d4157436dc6b70888b5bf3a34be2bcef8
Stanley Lee <sachih@live.com>
2020-01-22 21:39:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,9 +58,14 @@ total
 end
 
 def reduce_to_all_true(source_array)
-  if source_array 
-    return true 
-  else 
-    return nil 
+  i = 0 
+  
+  while i < source_array.length 
+    if source_array[i] == false 
+      return false 
+    end 
+    i += 1 
   end 
+  
+  return true
 end 
