362489bf60d6402af650d24e77f2f2693ae66d33
Stanley Lee <sachih@live.com>
2020-01-22 21:31:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,15 @@ end
 def map_to_no_change(source_array)
   source_array
 end 
+
+def map_to_double(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length 
+  
+  new_array.push(source_array[i] * source_array[i])
+  i+=1 
+end 
+
+new_array
+end
