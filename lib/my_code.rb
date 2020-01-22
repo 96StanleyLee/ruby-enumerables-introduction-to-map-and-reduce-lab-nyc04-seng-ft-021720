4f567a7f2f54e781395d9e812aa24787e410330a
Stanley Lee <sachih@live.com>
2020-01-22 21:41:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,9 +45,9 @@ end
 
 
 
-def reduce_to_total(source_array, i = 0)
+def reduce_to_total(source_array, starting_point)
   total = 0
-  while i < source_array.length 
+  while starting_point < source_array.length 
   total += source_array[i]
   
   i += 1 
